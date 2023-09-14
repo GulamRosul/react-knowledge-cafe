71e191a081db9ae6b7727bce29952b5935d7035b
rosol111 <gulamrosul037@gmail.com>
2023-09-15 01:31:27 +0600

    Remove bookmark after read

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,17 @@ function App() {
 
 
 
-  const handleMarkAsRead = time => {
+  const handleMarkAsRead = (id, time) => {
     const newReadingTime = readingTime + time;
     setReadingTime(newReadingTime);
+
+
+    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id !== id);
+    setBookmarks(remainingBookmarks);
   }
+
+  
+  
 
   return (
     <>
